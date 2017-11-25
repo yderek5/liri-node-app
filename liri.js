ca770e653f40354956086fc07ebc95d2f6ef1005
yderek5 <yderek5@gmail.com>
2017-11-25 13:11:10 -0600

    working on bug

--- liri.js
+++ liri.js
@@ -16,10 +16,44 @@ if (commandUsed.includes('my-tweets')) {
     access_token_key: twitterKeys.access_token_key,
     access_token_secret: twitterKeys.access_token_secret
   });
+  showTweets();
+}
+// ----------------------------------------------------
+// If User uses 'spotify-this-song' command
+else if (commandUsed.includes('spotify-this-song')) {
+  var spotify = new spotify({
+    id: '532f51e6f33a4fda80226df37c2f8f7e',
+    secret: '88f87df049f7476583f1ce85770ecaa8'
+  });
+  spotifyThis();
+}
+// ----------------------------------------------------
+// If User uses the 'movie-this' command
+else if (commandUsed.includes('movie-this')) {
+  movieThis();
+}
+// ----------------------------------------------------
+// If User uses the 'do-what-it-says' command
+else if (commandUsed.includes('do-what-it-says')) {
+  fs.readFile('random.txt', 'utf-8', function(err, res) {
+    if (err) {
+      return console.log(err);
+    }
+    commandUsed.push(res);
+    commandUsed = commandUsed.slice(1, commandUsed.length).join(',').split(',');
+    spotifyThis();
+    console.log(commandUsed);
+  });
+}
+// ----------------------------------------------------
+// FUNCTIONS
+function showTweets() {
   var params = {
     screen_name: 'nodejs'
   };
-  client.get('search/tweets', {q: 'yderek5'}, function(error, tweets, response) {
+  client.get('search/tweets', {
+    q: 'yderek5'
+  }, function(error, tweets, response) {
     if (!error) {
       console.log("Derek's tweets");
       console.log('------------------------');
@@ -32,14 +66,8 @@ if (commandUsed.includes('my-tweets')) {
     }
   });
 }
-// ----------------------------------------------------
-// If User uses 'spotify-this-song' command
-else if (commandUsed.includes('spotify-this-song')) {
-    var spotify = new spotify({
-    id: '532f51e6f33a4fda80226df37c2f8f7e',
-    secret: '88f87df049f7476583f1ce85770ecaa8'
-  });
 
+function spotifyThis() {
   var songTitle = commandUsed.slice(1, commandUsed.length);
   songTitle.join();
 
@@ -47,7 +75,10 @@ else if (commandUsed.includes('spotify-this-song')) {
     songTitle = 'The Sign Ace of Base';
   }
 
-  spotify.search({ type: 'track', query: songTitle }, function(err, data) {
+  spotify.search({
+    type: 'track',
+    query: songTitle
+  }, function(err, data) {
     if (err) {
       return console.log('Error occurred: ' + err);
     }
@@ -57,9 +88,8 @@ else if (commandUsed.includes('spotify-this-song')) {
     console.log(data.tracks.items[0].uri);
   });
 }
-// ----------------------------------------------------
-// If User uses the 'movie-this' command
-else if (commandUsed.includes('movie-this')) {
+
+function movieThis(movieTitle) {
   var apiKey = "&apikey=trilogy";
   var movieTitle;
   commandUsed = commandUsed.slice(1, commandUsed.length);
@@ -67,8 +97,8 @@ else if (commandUsed.includes('movie-this')) {
     for (var i = 0; i < commandUsed.length; i++) {
       movieTitle = commandUsed[i++] + '+' + commandUsed[i];
     }
-  } else if(commandUsed.length === 1) {
-      movieTitle = commandUsed.join();
+  } else if (commandUsed.length === 1) {
+    movieTitle = commandUsed.join();
   } else {
     movieTitle = 'mr+nobody';
   }
@@ -76,8 +106,12 @@ else if (commandUsed.includes('movie-this')) {
   var chosenMovie = "?t=" + movieTitle;
   var movieLink = "http://www.omdbapi.com/" + chosenMovie + apiKey;
 
-  request(movieLink, { json: true }, (err, res) => {
-  if (err) { return console.log(err); }
+  request(movieLink, {
+    json: true
+  }, (err, res) => {
+    if (err) {
+      return console.log(err);
+    }
     console.log('Movie Title: ' + res.body.Title);
     console.log('Year Movie Released ' + res.body.Year);
     console.log('IMDB Rating: ' + res.body.imdbRating);
@@ -88,15 +122,3 @@ else if (commandUsed.includes('movie-this')) {
     console.log('Actors/Actresses: ' + res.body.Actors);
   }); // end of request
 }
-// ----------------------------------------------------
-// If User uses the 'do-what-it-says' command
-  else if (commandUsed.includes('do-what-it-says')) {
-    fs.readFile('random.txt', 'utf-8', function(err, res) {
-      if(err) {
-        return console.log(err);
-      }
-      commandUsed = res;
-      console.log(res);
-      console.log(commandUsed);
-    });
-  } // No code below here
